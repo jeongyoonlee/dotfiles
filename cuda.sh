732e698981ee97645c2b0db4ea7bafe16758b345
Jeong-Yoon Lee <jeongyoon.lee1@gmail.com>
2016-01-05 05:00:54 +0000

    added -y for cuda installation

--- cuda.sh
+++ cuda.sh
@@ -1,4 +1,4 @@
 #wget http://developer.download.nvidia.com/compute/cuda/7.5/Prod/local_installers/cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404-7-5-local_7.5-18_amd64.deb
 sudo apt-get update
-sudo apt-get install cuda
+sudo apt-get install -y cuda
